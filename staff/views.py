f714fc7578b7d08ff696a21fd583e956699a938f
SANDHYA681 <sandhyatimsina438@gmail.com>
2025-07-28 11:12:57 +0545

    Worked on Online Class Inquiry

--- staff/views.py
+++ staff/views.py
@@ -2,28 +2,17 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .models import ClientVisit, OnlineClassInquiry, OfficeVisit, CollegeVisit
-
+from .forms import ClientVisitForm, OnlineClassInquiryForm, OfficeVisitForm, CollegeVisitForm
 
 #  DASHBOARD 
 
 @login_required
 def visit_dashboard(request):
-    if request.user.is_superuser:
-        client_visits = ClientVisit.objects.all().order_by('-id')
-        online_classes = OnlineClassInquiry.objects.all().order_by('-id')
-        office_visits = OfficeVisit.objects.all().order_by('-id')
-        college_visits = CollegeVisit.objects.all().order_by('-id')
-    else:
-        client_visits = ClientVisit.objects.filter(user=request.user).order_by('-id')
-        online_classes = OnlineClassInquiry.objects.filter(user=request.user).order_by('-id')
-        office_visits = OfficeVisit.objects.filter(user=request.user).order_by('-id')
-        college_visits = CollegeVisit.objects.filter(user=request.user).order_by('-id')
-
     return render(request, 'pages/staff/visit_dashboard.html', {
-        'client_visits': client_visits,
-        'online_classes': online_classes,
-        'office_visits': office_visits,
-        'college_visits': college_visits,
+        'client_form': ClientVisitForm(),
+        'online_form': OnlineClassInquiryForm(),
+        'office_form': OfficeVisitForm(),
+        'college_form': CollegeVisitForm(),
     })
 
 
@@ -32,62 +21,60 @@ def visit_dashboard(request):
 @login_required
 def add_client_visit(request):
     if request.method == 'POST':
-        ClientVisit.objects.create(
-            user=request.user,
-            name=request.POST.get('name'),
-            contact_number=request.POST.get('contact_number'),
-            purpose=request.POST.get('purpose'),
-            date=request.POST.get('date')
-        )
-        messages.success(request, "Client Visit added successfully.")
-        return redirect('client_visit_list')
+        form = ClientVisitForm(request.POST)
+        if form.is_valid():
+            client_visit = form.save(commit=False)
+            client_visit.user = request.user
+            client_visit.save()
+            messages.success(request, "Client Visit added successfully.")
+            return redirect('client_visit_list')
+        else:
+            messages.error(request, "There were errors in your form.")
     return redirect('visit_dashboard')
 
 
 @login_required
 def add_online_class(request):
     if request.method == 'POST':
-        OnlineClassInquiry.objects.create(
-            user=request.user,
-            name=request.POST.get('name'),
-            contact=request.POST.get('contact'),
-            purpose=request.POST.get('purpose'),
-            date=request.POST.get('date')
-        )
-        messages.success(request, "Online Class Inquiry added successfully.")
-        return redirect('online_class_list')
+        form = OnlineClassInquiryForm(request.POST)
+        if form.is_valid():
+            inquiry = form.save(commit=False)
+            inquiry.user = request.user
+            inquiry.save()
+            messages.success(request, "Online Class Inquiry added successfully.")
+            return redirect('online_class_list')
+        else:
+            messages.error(request, "There were errors in your form.")
     return redirect('visit_dashboard')
 
 
 @login_required
 def add_office_visit(request):
     if request.method == 'POST':
-        OfficeVisit.objects.create(
-            user=request.user,
-            name=request.POST.get('name'),
-            contact=request.POST.get('contact'),
-            email=request.POST.get('email'),
-            address=request.POST.get('address'),
-            purpose=request.POST.get('purpose'),
-            date=request.POST.get('date')
-        )
-        messages.success(request, "Office Visit added successfully.")
-        return redirect('office_visit_list')
+        form = OfficeVisitForm(request.POST)
+        if form.is_valid():
+            visit = form.save(commit=False)
+            visit.user = request.user
+            visit.save()
+            messages.success(request, "Office Visit added successfully.")
+            return redirect('office_visit_list')
+        else:
+            messages.error(request, "There were errors in your form.")
     return redirect('visit_dashboard')
 
 
 @login_required
 def add_college_visit(request):
     if request.method == 'POST':
-        CollegeVisit.objects.create(
-            user=request.user,
-            name=request.POST.get('name'),
-            contact_number=request.POST.get('contact'),
-            person_name=request.POST.get('person_name'),
-            purpose=request.POST.get('purpose')
-        )
-        messages.success(request, "College/School Visit added successfully.")
-        return redirect('college_visit_list')
+        form = CollegeVisitForm(request.POST)
+        if form.is_valid():
+            visit = form.save(commit=False)
+            visit.user = request.user
+            visit.save()
+            messages.success(request, "College/School Visit added successfully.")
+            return redirect('college_visit_list')
+        else:
+            messages.error(request, "There were errors in your form.")
     return redirect('visit_dashboard')
 
 

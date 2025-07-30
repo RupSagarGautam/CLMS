33aff2216f2d42148061021ac307588b5f8468f6
RupSagarGautam <rupsagargautam@gmail.com>
2025-07-30 14:11:09 +0545

    Update

--- cms/views.py
+++ cms/views.py
@@ -60,11 +60,6 @@ def logoutUser(request):
 
 @login_required(login_url="/log-in")
 def home(request):
-    if request.method == "GET":
-        print("Home Page")
-    else:
-        return redirect('/log-in')
-    return render(request, 'pages/home.html')
     if not request.user.is_authenticated:
         messages.error(request, "You need to log in to access this page")
         return render(request, "pages/login.html", status=403)

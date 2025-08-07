31bd6373f1d09c9279e701ec85de895dab8d21b1
SANDHYA681 <sandhyatimsina438@gmail.com>
2025-08-07 19:57:51 +0545

    set database

--- cms/settings.py
+++ cms/settings.py
@@ -122,7 +122,7 @@ DATABASES = {
 }
 
 
-# Password validationn
+# Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
